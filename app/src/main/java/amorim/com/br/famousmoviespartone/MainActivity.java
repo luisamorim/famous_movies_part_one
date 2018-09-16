5b7b0bd8264f95d7e41f1b981ca3ea67e6a58ad0
luis amorim <lamorim@scopus.com.br>
2018-09-16 11:23:17 -0300

    okhttp initial config tests

--- app/src/main/java/amorim/com/br/famousmoviespartone/MainActivity.java
+++ app/src/main/java/amorim/com/br/famousmoviespartone/MainActivity.java
@@ -1,13 +1,46 @@
 package amorim.com.br.famousmoviespartone;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.net.NetworkInterface;
+import java.net.URL;
+
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView mDisplayResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDisplayResult = (TextView) findViewById(R.id.tv_results);
+
+        makeMoviesQuery();
+    }
+
+    public void makeMoviesQuery(){
+        new FetchMoviesTask().execute(NetworkUtils.BASE_URL);
+    }
+
+    public class FetchMoviesTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return NetworkUtils.fetchMovies(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(s!=null && !s.equals("")){
+                mDisplayResult.setText(s);
+            }
+        }
     }
 }
--- app/src/main/java/amorim/com/br/famousmoviespartone/NetworkUtils.java
+++ app/src/main/java/amorim/com/br/famousmoviespartone/NetworkUtils.java
@@ -1,8 +1,37 @@
 package amorim.com.br.famousmoviespartone;
 
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * Created by luisamorim on 9/16/18.
  */
 
 public class NetworkUtils {
+
+    final static String API_KEY = "INSERT_YOUR_API_KEY_HERE";
+    final static String BASE_URL = "https://api.themoviedb.org/3/movie/550?api_key="+API_KEY;
+
+    public static String fetchMovies(String url){
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
 }
